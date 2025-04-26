74fde99e82a4538feed8719655e016e6b0ece115
joao neto <joaonetotfm@gmail.com>
2025-04-26 20:20:43 -0300

    feature: pds back

---
+++ src/test/java/com/cc/pds/PdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
